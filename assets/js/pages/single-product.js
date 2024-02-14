a46c85e8cb18f1c3fe38878d0b00d75b08fa8ab5
ElaheMahmoodi <elahemahmoodi81@gmail.com>
2024-02-14 10:17:56 +0330

    single product fixed

--- assets/js/pages/single-product.js
+++ assets/js/pages/single-product.js
@@ -1,11 +1,11 @@
 const btnShare = document.getElementById('btnShare');
-const titlePage = document.querySelector('#title').innerText;
+const titlePage = document.querySelector('#title').textContent;
 
 if (btnShare) {
   btnShare.addEventListener('click', () => {
     navigator.share({
       url: window.location.href,
-      title: titleBlog,
+      title: titlePage,
     });
   });
 }
