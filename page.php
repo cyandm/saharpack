97450ef0a88b0ea0dcc2ac6173fb6f0472e43623
ElaheMahmoodi <elahemahmoodi81@gmail.com>
2024-02-24 11:04:48 +0330

    cart completed

--- page.php
+++ page.php
@@ -1,13 +1,16 @@
 <?php get_header() ?>
 
-<main class="default-page container">
+<main class="default-page ">
     <div class="breadcrumb-wrapper">
         <div class="breadcrumb-product container">
             <?php if (function_exists('rank_math_the_breadcrumbs')) rank_math_the_breadcrumbs(); ?>
         </div>
         <i class="divider"></i>
     </div>
-    <?php the_content() ?>
+    <div class="container">
+
+        <?php the_content() ?>
+    </div>
 </main>
 
 <?php get_footer() ?>
