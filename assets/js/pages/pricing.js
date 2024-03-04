b0f85703a52c5acc7703b4c59a77ec4650f30eb9
amirali-dizabadi <amirali.dizabadi@gmail.com>
2024-03-04 19:00:06 +0330

    v 1.0.1.0 fix mouse and pricing form and mobile menu

--- assets/js/pages/pricing.js
+++ assets/js/pages/pricing.js
@@ -1,53 +1,54 @@
-import { errorToast, successFormToast } from '../modules/toastify';
-import { toggleActivateEl } from '../utils/functions';
+import { errorToast, successFormToast } from "../modules/toastify";
+import { toggleActivateEl } from "../utils/functions";
 
 const Pricing = () => {
-	const pricingCollapseHandle = document.getElementById(
-		'pricingCollapseHandle'
-	);
+  const pricingCollapseHandle = document.getElementById(
+    "pricingCollapseHandle"
+  );
 
-	const pricingCollapse = document.getElementById('pricingCollapse');
+  const pricingCollapse = document.getElementById("pricingCollapse");
 
-	if (!pricingCollapse || !pricingCollapseHandle) return;
+  if (!pricingCollapse || !pricingCollapseHandle) return;
 
-	pricingCollapseHandle.addEventListener('click', () => {
-		toggleActivateEl(pricingCollapse);
-	});
+  pricingCollapseHandle.addEventListener("click", () => {
+    toggleActivateEl(pricingCollapse);
+  });
 };
 
 Pricing();
 
 const PricingForm = () => {
-	const priceForm = document.getElementById('priceForm');
-	if (!priceForm) return;
+  const priceForm = document.getElementById("priceForm");
+  if (!priceForm) return;
 
-	const submitBtn = priceForm.querySelector('button[type="submit"]');
-	if (!submitBtn) return;
+  const submitBtn = priceForm.querySelector('button[type="submit"]');
+  if (!submitBtn) return;
 
-	priceForm.addEventListener('submit', (e) => {
-		e.preventDefault();
-		const formData = new FormData(e.currentTarget, e.submitter);
-		formData.append('_nonce', cyn_head_script.nonce);
-		formData.append('action', 'cyn_pricing_form');
+  priceForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget, e.submitter);
+    formData.append("_nonce", cyn_head_script.nonce);
+    formData.append("action", "cyn_pricing_form");
 
-		jQuery(($) => {
-			$.ajax({
-				url: cyn_head_script.url,
-				type: 'post',
-				cache: false,
-				processData: false,
-				contentType: false,
-				data: formData,
-				success: (res) => {
-					successFormToast.showToast();
-					priceForm.reset();
-				},
-				error: () => {
-					errorToast.showToast();
-				},
-			});
-		});
-	});
+    jQuery(($) => {
+      $.ajax({
+        url: cyn_head_script.url,
+        type: "post",
+        cache: false,
+        processData: false,
+        contentType: false,
+        data: formData,
+        success: (res) => {
+          console.log(res);
+          successFormToast.showToast();
+          priceForm.reset();
+        },
+        error: () => {
+          errorToast.showToast();
+        },
+      });
+    });
+  });
 };
 
 PricingForm();
