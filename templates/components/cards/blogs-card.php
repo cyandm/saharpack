d2cca17d621b2545ad6800cc67161c5064a05733
elahe <elahemahmoodi81@gmail.com>
2024-03-09 00:36:37 +0330

    some font-size restyled

--- templates/components/cards/blogs-card.php
+++ templates/components/cards/blogs-card.php
@@ -3,19 +3,24 @@ $post_id = isset($args['post_id']) ? $args['post_id'] : get_the_ID();
 $author_name = get_the_author_meta('display_name', get_post_field('post_author', $post_id));
 ?>
 <a href="<?php the_permalink($post_id) ?>">
-	<div class="blog-posts blog-container">
-		<div class="post-thumbnail">
-			<?= get_the_post_thumbnail($post_id, 'full') ?>
-		</div>
-		<div class="post-detail">
-			<div class="postmeta">
-				<div class="postmeta-r">
-					<span class="meta-date meta"> <i class="iconsax" icon-name="calendar-2"></i><?= get_the_date() ?></span>
-					<span class="meta-author meta"><i class="iconsax" icon-name="edit-1"></i><?= $author_name ?></span>
-				</div>
-			</div>
-			<h5><?= get_the_title($post_id) ?></h5>
-			<div class="paragraph"> <?= get_the_content() ?></div>
-		</div>
-	</div>
+    <div class="blog-posts blog-container">
+        <div class="post-thumbnail">
+            <?php if (!empty(get_the_post_thumbnail($post_id))) : ?>
+            <?= get_the_post_thumbnail($post_id, 'full') ?>
+            <? else : ?>
+            <img src="<?= get_stylesheet_directory_uri() . '/assets/img/placeholder.png' ?>" />
+            <?php endif ?>
+        </div>
+        <div class="post-detail">
+            <div class="postmeta">
+                <div class="postmeta-r">
+                    <span class="meta-date meta"> <i class="iconsax"
+                            icon-name="calendar-2"></i><?= get_the_date() ?></span>
+                    <span class="meta-author meta"><i class="iconsax" icon-name="edit-1"></i><?= $author_name ?></span>
+                </div>
+            </div>
+            <h5><?= get_the_title($post_id) ?></h5>
+            <div class="paragraph"> <?= get_the_content() ?></div>
+        </div>
+    </div>
 </a>
