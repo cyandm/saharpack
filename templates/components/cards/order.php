4c84038fa5e6df6e33fe00f1f940fec34c553677
ElaheMahmoodi <elahemahmoodi81@gmail.com>
2024-03-12 17:40:22 +0330

    my order page translate added

--- templates/components/cards/order.php
+++ templates/components/cards/order.php
@@ -1,14 +1,7 @@
 <?php
 $order = $args['order'];
-echo '<pre>';
-//var_dump($order['line_items']);
-
-// foreach ($order['line_items'] as $key => $order_prop) {
-//     var_dump(wc_get_email_order_items($order, []));
-// }
 $total_items = 0;
-echo '</pre>';
-//var_dump($order->get_items()[101]);
+
 foreach ($order->get_items() as $item_id => $item) {
     $total_items += $item->get_quantity();
 }
@@ -25,24 +18,24 @@ foreach ($order->get_items() as $item_id => $item) {
 
     <div class="order-box">
         <div class="odrer-title">
-            <h5><?= pll__('کد سفارش : ') ?><?= $order->data['id'] ?></h5>
+            <h5><?= pll__('order-code') . ' : ' ?><?= $order->data['id'] ?></h5>
         </div>
         <div class="order-detail">
             <div class="order-details-row">
-                <span><?= pll_e('تعداد') ?></span>
+                <span><?= pll_e('order-count') ?></span>
                 <span><?= $total_items ?></span>
             </div>
             <div class="order-details-row">
-                <span><?= pll__('قیمت کل') ?></span>
+                <span><?= pll__('total-price') ?></span>
                 <span><?= $order->get_total() ?> <?= pll__('تومان') ?></span>
             </div>
             <div class="order-details-row">
-                <span><?= pll__('تاریخ سفارش') ?> </span>
+                <span><?= pll__('order-date') ?> </span>
 
                 <span class="order-date"><?= wc_format_datetime($order->get_date_created()) ?></span>
             </div>
             <div class="order-details-row">
-                <span><?= pll__('وضعیت سفارش') ?></span>
+                <span><?= pll__('order-status') ?></span>
                 <span class="status send"><?= wc_get_order_status_name($order->data['status']) ?></span>
             </div>
         </div>
